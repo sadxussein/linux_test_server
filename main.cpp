c4303747024eb78a9089aee7a402b4ffd5a21ed0
sadxussein <kvdworking@gmail.com>
2023-06-05 00:16:32 +0300

    added some libs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>	// system calls data types
 #include <sys/socket.h>	// socket structures
 #include <netinet/in.h>	// internet domain structures
